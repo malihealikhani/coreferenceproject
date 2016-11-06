227c6967ae9ee904a5bcc2a68ed634bc82ad3bd7
Sean Allen <u0021994@utah.edu>
2016-11-05 22:04:59 -0600

    Worked on adding in a lot of functionality
    Added setence and word tokenization. Also added POS tagging. Working on
    lexical similarity, gender/number/person agreement, and substring and
    partial matches.

--- coreference.py
+++ coreference.py
@@ -1,4 +1,4 @@
-########################################################
+################################################################################
 #
 # Sean Allen - U0021994
 # Nick Sullivan -
@@ -7,11 +7,14 @@
 # Term Project - Co-Reference Resolver
 # coreference.py
 #
-########################################################
+################################################################################
 
 
-
+# XML Library
+import xml.etree.ElementTree as ET
 import sys
+import nltk
+import glob
 
 
 ################################################################################
@@ -22,15 +25,18 @@ import sys
 # Returns:
 # Notes:
 #
+#
 ################################################################################
 
 
 def main(files):
 
-    g, s = files
+    # This grabs all the .crf files from the dev folder. We can iterate through these.
+    # Just using one for testing though (a8.crf)
+    files = glob.glob("./dev/*.crf")
 
-    with open('parser.trace', 'w') as f:
-        f.write( coreference() )
+    with open('coreference.trace', 'w') as f:
+        f.write(coreference(files[3]))
 
 
 ##################################################################################
@@ -39,12 +45,96 @@ def main(files):
 #
 # Parameters:
 # Returns:
-# Notes:
+# Notes: The slides on co-reference contain the following: Typical Co-reference
+# Pipeline -Preprocessor: XML removal, tokenization, sentence and paragraph
+# splitting -Part of Speech Tagging -Parsing -Named Entity Recognizer
+# -Semantic Class Lookup (usualy via WordNet) -Candidate NP extraction
+# -Supervised Learning -Clustering into Chains
 #
 ##################################################################################
 
-def coreference():
+def coreference(f):
+
+    # Create data structure
+    tree = ET.parse(open(f))
+    tree_root = tree.getroot()
+    coref = create_coref(tree_root)
+    notags = ET.tostring(tree_root, encoding='utf8', method='text')
+    text = nltk.word_tokenize(notags)  # breaks up all the words into tokens and puts into a list
+    sentences = nltk.sent_tokenize(notags)  # breaks corpus into sentences
+    pos_tags = nltk.pos_tag(text)  # tags all the words in the corpus
+    tags = ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'PRP$']
+    nouns = [noun for noun in pos_tags if noun[1] in tags]  # just the nouns, pronouns
+
+    # check exact match in other corefs
+    coref = checkExactMatch(coref)
+
+    # check acronyms
+    coref = checkAcronym(coref)
+
+    # check head noun match
+    # check substring and partial match
+    # check word overlap comparisons
+    #
+    # Check Lexical Similarity
+    # i.e. Ford Motor Company = Ford Co. = Ford
+    # i.e. Apple Computer = Apple
+    # i.e. Federabl Bureau of Investigation = FBI
+
+    # Look through candidate list of Nouns checking using
+    # scoping heuristics and recency
+    # number, person, and gender agreement
+    # sytactic heuristics
+    # semantic compatability
+
     return "This needs to be updated\n"
+
+
+def create_coref(root):
+    coref = []  # I changed this to a list so we can keep the order
+    for child in root:
+        text = child.text
+
+        # removes new lines in the middle of a string
+        if text.__contains__("\n"):
+            text = text[:text.index("\n")] + " " + text[text.index("\n")+1:]
+
+        coref.append([str(child.get('ID')), text.lower()])
+    return coref
+
+
+def checkExactMatch(coref):
+    cnt = 0
+    for i in range(len(coref)):
+        for j in range(i, -1, -1):
+            if coref[i][1] == coref[j][1] and coref[i][0] != coref[j][0]:
+                if len(coref[i]) is 2:
+                    coref[i].append(coref[j][0])
+                    cnt += 1
+                    break
+    return coref
+
+
+def checkAcronym(coref):
+    for i in range(len(coref)):
+        if len(coref[i]) is 2:
+            acr = ""
+            # This finds references from acronyms (FAA finds Federal Aviation Administration)
+            if len(coref[i][1].split(" ")) is 1:
+                for j in range(i, -1, -1):
+                    if len(coref[j][1].split(" ")) is len(coref[i][1]):
+                        for word in coref[j][1].split():
+                            acr += word[0]
+                        if acr is coref[i][1]:
+                            coref[i].append(coref[j][0])
+            # This finds acronyms from references (Federal Aviation Administration finds FAA)
+            else:
+                for word in coref[i][1].split():
+                    acr += word[0]
+                for j in range(i, -1, -1):
+                    if coref[j][1] is acr:
+                        coref[i].append(coref[j][0])
+    return coref
 
 
 # Execution script
