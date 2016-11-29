e3e2833f8675bb15261290ca810e4f80048110e3
ncarsonsullivan <ncarsonsullivan@gmail.com>
2016-11-28 17:44:49 -0700

    Update coreference.py

--- coreference.py
+++ coreference.py
@@ -446,6 +446,15 @@ def checkNotTagged(coref,notref):
             'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
             'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
             'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+def checkNotTagged(coref,notref):
+
+    cnt = 1
+    skip = ['what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+            'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+            'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+            'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+            'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
             'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
             'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
             'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
@@ -461,7 +470,7 @@ def checkNotTagged(coref,notref):
                 for word in words:
                     #print "word is: " + word
                     if not (word.lower() in skip):
-                        inText = notref.lower.find(word.lower())
+                        inText = notref.lower().find(word.lower())
 
                         if inText > 0:
                             found = True
